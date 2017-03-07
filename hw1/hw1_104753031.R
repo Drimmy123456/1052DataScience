80c7baa2c56d3ccec3f02c287ba805e968854750
Drimmy123456 <104753031@nccu.edu.tw>
2017-03-07 19:54:43 +0800

    test

--- hw1/hw1_104753031.R
+++ hw1/hw1_104753031.R
@@ -10,16 +10,46 @@ if (length(args)==0) {
 }
 
 i <- 1
+i_f <- c()
 while(i < length(args)) {
-  if (args[i]=="-files") {
-    i_f <- args[i+1]
+  if ( args[i] == "-files") {
+    i <- i+1
+    j <- 1
+    while(!startsWith(args[i], "-")&&(i <= length(args))) {
+      i_f[j] <- args[i]
+      i <- i+1
+      j <- j+1
+    }
+    
+    
   }
-  if (args[i]=="-out") {
+  if (args[i] == "-out") {
     o_f <- args[i+1]
   }
   i <- i+1
 }
 
 
-print(i_f)
-print(o_f)
+
+set <- c()
+weight <- c()
+height <- c()
+
+
+for (i in c(1:length(i_f))) {
+  f <- i_f[i]
+  set_name <- substr(f, 1, nchar(f)-4)
+  set <- append(set,set_name)
+  data <- read.csv(f)
+  weight <- append(weight,round(max(data["weight"]),digit=2))
+  height <- append(height,round(max(data["height"]),digit=2))
+  
+}
+
+out <- data.frame(set, weight, height)
+
+
+write.csv(out, file=o_f, row.names=FALSE)
+#print(set)
+#print(i_f)
+#print(out)
