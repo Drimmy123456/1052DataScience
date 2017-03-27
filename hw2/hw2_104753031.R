f9d21b65fd5da9755b59ccc618dd47838820ab3c
Drimmy123456 <104753031@nccu.edu.tw>
2017-03-27 22:58:30 +0800

    hw2

--- hw2/hw2_104753031.R
+++ hw2/hw2_104753031.R
@@ -1,61 +1,79 @@
-query_func<-function(query_m, i)
-{
-  if(query_m == "male"){
-    which.min(i)
-  }
-  else if (query_m == "female") {
-    which.max(i)
-  } else {
-    stop(paste("ERROR: unknown query function", query_m))
-  }
-}
-
 # read parameters
 args = commandArgs(trailingOnly=TRUE)
 if (length(args)==0) {
-  stop("USAGE: Rscript hw1.R -query min|max -files file1 file2 ... filen ?€“out out.csv", call.=FALSE)
+  stop("USAGE: Rscript hw2_104753031.R --target male/female --files meth1 meth2 â€¦ methx --out result.csv", call.=FALSE)
 }
 
 # parse parameters
 i<-1 
 while(i < length(args))
 {
-  if(args[i] == "-query"){
-    query_m<-args[i+1]
+  if(args[i] == "--target"){
+    target<-args[i+1]
     i<-i+1
-  }else if(args[i] == "-files"){
+  }else if(args[i] == "--files"){
     j<-grep("-", c(args[(i+1):length(args)], "-"))[1]
     files<-args[(i+1):(i+j-1)]
     i<-i+j-1
-  }else if(args[i] == "-out"){
+  }else if(args[i] == "--out"){
     out_f<-args[i+1]
     i<-i+1
   }else{
-    stop(paste("Unknown flag", args[i]), call.=FALSE)
+    stop(paste("Unknown flag: ", args[i]), call.=FALSE)
   }
   i<-i+1
 }
 
 print("PROCESS")
-print(paste("query mode :", query_m))
+print(paste("target     :", target))
 print(paste("output file:", out_f))
 print(paste("files      :", files))
 
 # read files
-names<-c()
-weis<-c()
-heis<-c()
+ms<-c()
+sen<-c()
+sep<-c()
+f1<-c()
+auc<-c()
 for(file in files)
 {
-  name<-gsub(".csv", "", basename(file))
+  # read data
   d<-read.table(file, header=T,sep=",")
-  weis<-c(weis, d$weight[query_func(query_m, d$weight)])
-  heis<-c(heis, d$height[query_func(query_m, d$height)])
-  names<-c(names,name)
+  
+  # record method name
+  name<-gsub(".csv", "", basename(file))
+  ms<-c(ms,name)
+  
+  # compute confusion table,TP,TN,FP,FN
+  cM <- table(truth=d$reference, prediction=d$prediction)
+  if(target == "male"){
+    TP<-cM[["male","male"]]
+    TN<-cM[["female","female"]]
+    FP<-cM[["female","male"]]
+    FN<-cM[["male","female"]]
+  }else if(target == "female"){
+    TP<-cM[["female","female"]]
+    TN<-cM[["male","male"]]
+    FP<-cM[["male","female"]]
+    FN<-cM[["female","male"]]
+  }else{
+    stop(paste("Unknown target: ", target), call.=FALSE)
+  }
+  
+  # compute sensitivity, specificity, F1, AUC
+  sen<-c(sen,TP/(TP+FN))
+  sep<-c(sep,TN/(TN+FP))
+  f1<-c(f1,2*TP/(2*TP+FP+FN))
+  
+  # compute AUC
+  library('ROCR')
+  eval <- prediction(d$pred.score,d$reference)
+  value<-attributes(performance(eval,'auc'))$y.values[[1]]
+  auc<-c(auc,value)
 }
-out_data<-data.frame(set=names, wei=weis, hei=heis, stringsAsFactors = F)
-index<-sapply(out_data[,c("wei","hei")], query_func, query_m=query_m)
+out_data<-data.frame(method=ms, sensitivity=sen, specificity=sep, F1=f1, AUC=auc, stringsAsFactors = F)
+index<-sapply(out_data[,c("sensitivity","specificity","F1","AUC")], which.max)
 
 # output file
-out_data<-rbind(out_data,c(query_m,names[index]))
-write.table(out_data, file=out_f, row.names = F, quote = F)
+out_data<-rbind(out_data,c(highest,ms[index]))
+write.table(out_data, file=out_f, row.ms = F, quote = F)
