6970bea8d1b244f50ed348d2b59f70e88b70d206
Drimmy123456 <104753031@nccu.edu.tw>
2017-04-24 19:14:35 +0800

    hw3

--- hw2/hw3_104753031.R
+++ hw2/hw3_104753031.R
@@ -1,7 +1,13 @@
+library('ROCR')
+# defind function: which.second(x)
+which.second<-function(x){
+  sort.int(x,decreasing = TRUE,index.return = TRUE)$ix[2]
+}
+
 # read parameters
 args = commandArgs(trailingOnly=TRUE)
 if (length(args)==0) {
-  stop("USAGE: Rscript hw2_104753031.R --target male/female --files meth1 meth2 ... methx --out result.csv", call.=FALSE)
+  stop("USAGE: Rscript hw3_104753031.R --target male/female --files meth1 meth2 ... methx --out result.csv", call.=FALSE)
 }
 
 # parse parameters
@@ -35,6 +41,9 @@ sen<-c()
 sep<-c()
 f1<-c()
 auc<-c()
+correct<-c()
+incorrect<-c()
+
 for(file in files)
 {
   # read data
@@ -44,29 +53,42 @@ for(file in files)
   name<-gsub(".csv", "", basename(file))
   ms<-c(ms,name)
   
-  # compute confusion table,TP,TN,FP,FN
-  cM <- table(truth=d$reference, prediction=d$prediction)
+  # compute TP,TN,FP,FN
   if(target == "male"){
-    TP<-cM[["male","male"]]
-    TN<-cM[["female","female"]]
-    FP<-cM[["female","male"]]
-    FN<-cM[["male","female"]]
+    not_target<-"female"
   }else if(target == "female"){
-    TP<-cM[["female","female"]]
-    TN<-cM[["male","male"]]
-    FP<-cM[["male","female"]]
-    FN<-cM[["female","male"]]
+    not_target<-"male"
   }else{
     stop(paste("Unknown target: ", target), call.=FALSE)
   }
   
-  # compute sensitivity, specificity, F1, AUC
+  TP<-0
+  TN<-0
+  FP<-0
+  FN<-0
+  i<-1 
+  while(i < length(d$reference)){
+    if(d$reference[i]==target && d$prediction==target){
+      TP<-TP+1
+    }else if(d$reference[i]==not_target && d$prediction==not_target){
+      TN<-TN+1
+    }else if(d$reference[i]==not_target && d$prediction==target){
+      FP<-FP+1
+    }else if(d$reference[i]==target && d$prediction==not_target){
+      FN<-FN+1
+    }
+    
+    
+    i<-i+1
+  }
+  
+  # compute sensitivity, specificity, F1
   sen<-c(sen,round(TP/(TP+FN),digit=2))
   sep<-c(sep,round(TN/(TN+FP),digit=2))
   f1<-c(f1,round(2*TP/(2*TP+FP+FN),digit=2))
   
   # compute AUC
-  library('ROCR')
+  
   if(target == "male"){
     pred<-d$pred.score
   }else if(target == "female"){
@@ -77,10 +99,53 @@ for(file in files)
   eval <- prediction(pred,d$reference)
   value<-attributes(performance(eval,'auc'))$y.values[[1]]
   auc<-c(auc,round(value,digit=2))
+  
+  # collect the number of correct
+  correct<-c(correct,TP+TN)
+  incorrect<-c(incorrect,FP+FN)
+
 }
 out_data<-data.frame(method=ms, sensitivity=sen, specificity=sep, F1=f1, AUC=auc, stringsAsFactors = F)
-index<-sapply(out_data[,c("sensitivity","specificity","F1","AUC")], which.max)
+
+# find index of the max and the second value to each score
+index_max<-sapply(out_data[,c("sensitivity","specificity","F1","AUC")], which.max)
+index_second<-sapply(out_data[,c("sensitivity","specificity","F1","AUC")], which.second)
+
+print(index_max)
+print(index_second)
+
+# contingency table and significance test
+star<-c()
+i<-1
+while(i < 5){
+  d <- data.frame(c(correct[[index_max[[i]]]],correct[[index_second[[i]]]]),c(incorrect[[index_max[[i]]]]),incorrect[[index_second[[i]]]])
+
+  
+  
+  if(fisher.test(d)$p.value<0.05){
+    star<-c(star,TRUE)
+  }else{
+    star<-c(star,FALSE)
+  }
+  
+  print(fisher.test(d)$p.value)
+  
+  i<-i+1
+}
+
+
 
 # output file
-out_data<-rbind(out_data,c("highest",ms[index]))
+last_row<-c("highest",ms[index_max])
+i<-2
+while(i < 6){
+  if(star[i-1]==TRUE){
+    last_row[i]<-paste0(last_row[i],"*")
+  }
+  
+  i<-i+1
+}
+
+out_data<-rbind(out_data,last_row)
 write.table(out_data, file=out_f, quote = F, sep = ",", row.names=F)
+
